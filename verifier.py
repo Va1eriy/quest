34d75fe67b439a0b2eea8d623effea02d95782ae
sofus <little.quibbler@gmail.com>
2021-06-19 01:03:58 +0800

    adding first quest

---
+++ verifier.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import redirect, request, abort, send_from_directory
+
+
+app = Flask(__name__, template_folder='.')
+
+
+quests = {
+    1: {"key": "156", "next_page": "task2.html"},
+    2: {"key": "ede", "next_page": "final.html"}
+}
+
+@app.route('/')
+def render_main():
+    return send_from_directory(".", "index.html")
+
+
+@app.route('/<path:filename>')
+def send_file(filename):
+      return send_from_directory('.', filename)
+
+
+@app.route('/<int:quest_id>/')
+def verify(quest_id):
+    presented_key = request.args.get('key')
+
+    if quest_id not in quests:
+        abort(404, description="Resource not found")
+
+    quest = quests[quest_id]
+    if presented_key == quest["key"]:
+        return quest["next_page"]
+        #return send_from_directory('.', quest["next_page"])
+    else:
+        return "Wrong key", 400
+
+if __name__ == '__main__':
+    app.run()
