b4970029a53423d924d4569d5d1514b5b40c4fc1
sofus <little.quibbler@gmail.com>
2021-06-20 13:31:08 +0800

    Try check

--- verifier.py
+++ verifier.py
@@ -8,9 +8,15 @@ CORS(app)
 
 
 quests = {
-    1: {"key": "156", "next_page": "task2.html"},
+    1: {"key": "156", "hint": "Первая цифра - 1","next_page": "task2.html"},
     2: {"key": "маракуйя", "next_page": "task3.html"},
     3: {"key": "ede", "next_page": "final.html"}
+}
+
+quests_try= {
+    1: 0,
+    2: 0,
+    3: 0
 }
 
 @app.route('/')
@@ -36,7 +42,18 @@ def verify(quest_id):
         return quest["next_page"]
         #return send_from_directory('.', quest["next_page"])
     else:
+        print(f"log: Q {quest_id} TRY {quests_try[quest_id]}")
+        if  "hint" in quest and quests_try[quest_id] >= 5:
+            return quest["hint"], 400
+        quests_try[quest_id] = quests_try[quest_id]+1
         return "Wrong key", 400
+
+@app.route('/reset/')
+def reset_try():
+    for quest_id in quests_try:
+        print(f"log: Q {quest_id} WAS {quests_try[quest_id]} --> 0")
+        quests_try[quest_id] = 0
+    return "Success"
 
 if __name__ == '__main__':
     app.run()
